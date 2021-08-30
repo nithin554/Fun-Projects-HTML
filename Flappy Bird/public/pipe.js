00c82c956f8441fac27be0a4e4089d218dfdfeac
Nithin <nithinneeraj60@gmail.com>
2021-08-30 14:51:22 +0530

    Add files via upload

---
+++ Flappy Bird/public/pipe.js
@@ -0,0 +1,40 @@
+class Pipe{
+    constructor(x,y,height,dir){
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.dir = dir;
+        this.passed = false;
+    }
+    draw(){
+        if(x<0){
+            ctx.clearRect(this.x+1.5,this.y,20,this.height);
+            return;
+        }
+        ctx.clearRect(this.x+1.5,this.y,21,this.height);
+        ctx.fillRect(this.x, this.y, 20, this.height);
+    }
+    hits(dy){
+        if(dy+(this.dir*15) >= this.y && dy+(this.dir*15) <= this.height + this.y){
+            if(this.x <= 65 && this.x >= 15){
+                ctx.fillStyle = 'red'
+                ctx.fillRect(this.x, this.y, 22, this.height);
+                return true;
+            }
+            else{
+                if(this.x < 15 && !this.passed){
+                    score += 0.5
+                    this.passed = true;
+                }
+                return false;
+            }
+        }
+        else{
+            if(this.x < 15 && !this.passed){
+                score += 0.5;
+                this.passed = true;
+            }
+            return false;
+        }
+    }
+}
